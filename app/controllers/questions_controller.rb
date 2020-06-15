c6cedfb8696f3eb0ee99a18a846ea00908e87f56
cd ~/code/$GITHUB_USERNAME/dotfiles <vitorccastro@gmail.com>
2020-06-15 15:12:21 +0100

    rails new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,14 @@
 class QuestionsController < ApplicationController
   def ask
   end
+  def answer
+    @question = params[:question]
+    if @question == 'I am going to work'
+      @answer = "bom para ti"
+    elsif @question.last == "?"
+      @answer = "asdfadsf"
+    else
+      @answer = "i dont care, get dressed and go to work"
+    end
+  end
 end
